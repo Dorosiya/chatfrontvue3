c58cb84741be0b0d0f4839bdcd89f633b3e59f90
shyu <shyu6370@gmail.com>
2025-05-12 01:21:24 +0900

    first commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+
+export default createVuetify (
+
+    {
+        components
+    }
+
+)
+
